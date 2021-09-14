05d89db970f72c5b4b82f07594ad2d8a36e3869c
Hannes Barbez <hannesbarbez@hotmail.com>
2021-09-14 18:26:24 +0200

    Bumps version;

--- StockTwitsDataProvider.cs
+++ StockTwitsDataProvider.cs
@@ -83,15 +83,19 @@ namespace BarbezDotEu.StockTwits
             var queryUrl = $"{this.Configuration.SearchRecentTwitsUrl}{symbol.ToUpperInvariant()}{this.Configuration.SearchRecentTwitsFields}";
             var request = new HttpRequestMessage(HttpMethod.Get, queryUrl);
             request.Headers.Accept.Add(acceptHeader);
+            return await this.Request<StockTwitsResponse>(request, retryOnError, waitingMinutesBeforeRetry);
+        }
+
+        /// <inheritdoc/>
+        protected override Task<PoliteReponse<T>> Request<T>(HttpRequestMessage request, bool retryOnError = true, double waitingMinutesBeforeRetry = 15)
+        {
             try
             {
-                var result = await this.Request<StockTwitsResponse>(request, retryOnError, waitingMinutesBeforeRetry);
-                return result;
+                return base.Request<T>(request, retryOnError, waitingMinutesBeforeRetry);
             }
             catch (JsonException e)
             {
-                // TODO: Move try/catch to BarbezDotEu.Provider instead.
-                base.Logger.LogWarning($"An error occurred that we're going to ignore since occasionally, StockTwits sends back XML/HTML responses. Moving on from: {e}");
+                base.Logger.LogWarning($"An error occurred that we're going to ignore since occasionally, StockTwits sends back empty responses. Moving on from: {e}");
                 return null;
             }
         }
